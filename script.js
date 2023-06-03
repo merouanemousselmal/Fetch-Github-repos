2dc712177d25691d8ae1abbe27284c6c5a12fa39
MerouaneMousselmal <merouane.mousselmal@gmail.com>
2023-06-03 15:39:48 +0100

    commit

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ const fetchData = async function () {
     );
     const data = await res.json();
     if (res.ok) {
-      console.log("secces", data);
+      //console.log("secces", data);
       result.innerHTML = "";
       data.forEach((el) => {
         const markup = `<span class="inforesult">${el.name}  <span>${el.forks} /  ${el.visibility} / ${el.id} / <a href=${el.html_url}>Visit</a></span></span>`;
         result.insertAdjacentHTML("beforebegin", markup);
       });
     } else {
-      console.log("serveur error", data);
+      return;
     }
   } catch (err) {
     console.log(err);
